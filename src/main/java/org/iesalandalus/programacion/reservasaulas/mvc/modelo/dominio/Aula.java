bf83f301cac31a69b5f58a35ac2d211d5021bbba
juig_ <juig_@DESKTOP-0OPHGMB>
2022-03-13 13:15:06 +0100

    commit punto 3

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Aula.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Aula.java
@@ -3,22 +3,28 @@ package org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio;
 import java.util.Objects;
 
 public class Aula {
-
+	static final float PUNTOS_POR_PUESTO = 0.5f;
+	static final int MAX_PUESTOS = 50;
+	static final int MIN_PUESTOS = 10;
 	private String nombre;
+	private int puesto;
 	
-	public Aula(String nombre) {
+	public Aula(String nombre, int puesto) {
 		setNombre(nombre);
+		setPuesto(puesto);
 	}
 	
 	public Aula(Aula aula) {
 		if(aula == null)
 			throw new NullPointerException("ERROR: No se puede copiar un aula nula.");
 		setNombre(aula.getNombre());
+		setPuesto(aula.getPuestos());
 	}
+	
 	private void setNombre(String nombre) {
 		if(nombre == null)
 			throw new NullPointerException("ERROR: El nombre del aula no puede ser nulo.");
-		if(nombre == "")
+		if(nombre.trim().isEmpty())
 			throw new IllegalArgumentException("ERROR: El nombre del aula no puede estar vacío.");
 		this.nombre = nombre;
 	}
@@ -26,7 +32,25 @@ public class Aula {
 	public String getNombre() {
 		return this.nombre;
 	}
+	
+	public int getPuestos() {
+		return this.puesto;
+	}
 
+	public void setPuesto(int puesto) {
+		if(puesto>MAX_PUESTOS | puesto<MIN_PUESTOS)
+			throw new IllegalArgumentException("ERROR: El número de puestos no es correcto.");
+		this.puesto = puesto;
+	}
+	
+	public float getPuntos() {
+		return getPuestos()*PUNTOS_POR_PUESTO;
+	}
+	
+	public Aula getAulaFicticia(String nombre) {
+		return new Aula(nombre, 1);
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(nombre);
@@ -46,7 +70,7 @@ public class Aula {
 
 	@Override
 	public String toString() {
-		return "nombre Aula=" + nombre;
+		return "nombre=" + nombre + ", puestos=" + puesto;
 	}
 	
 }
